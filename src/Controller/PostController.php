3b1e7c906fd358d0d7d29e3d206bf5b41a2acd32
faro3328 <faro3328@gmail.com>
2022-08-31 14:59:22 +0200

    handle post twig controller

---
+++ src/Controller/PostController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Form\PostType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class PostController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        return $this->render('post/index.html.twig', [
+            "age" => 17
+        ]);
+    }
+
+    #[Route('/post/new')]
+    public function create(Request $request): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            dump($post);
+        }
+        return $this->render('post/form.html.twig', [
+            "post_form" => $form->createView()
+        ]);
+    }
+}
+
+
+?>
+<!-- terminal composer self-update -->
+<!-- terminal php bin/console make:controller "nom du controler" -->
